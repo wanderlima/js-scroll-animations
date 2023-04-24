2b981f4a144f1ceeb3865060be085f82c25c7750
wanderlima <wander@wanderlima.com>
2023-04-24 19:54:50 -0300

    clean up and demo page improvement

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,12 @@ const minify = require("gulp-minify");
 const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require("gulp-autoprefixer");
 const sass = require("gulp-sass")(require("sass"));
-var browserify = require("browserify");
-var log = require("gulplog");
-var tap = require("gulp-tap");
-var buffer = require("gulp-buffer");
-var uglify = require("gulp-uglify");
+const browserify = require("browserify");
+const log = require("gulplog");
+const tap = require("gulp-tap");
+const buffer = require("gulp-buffer");
+const uglify = require("gulp-uglify");
+var argv = require("yargs").argv;
 
 const SRC_PATH = path.join(__dirname, "src");
 const BUILD_PATH = path.join(__dirname, "dist");
@@ -21,28 +22,37 @@ function clean(cb) {
   cb();
 }
 
-function cssBuild(cb) {
-  return (
-    gulp
-      .src(SRC_PATH + "/**/*.scss")
-      .pipe(sourcemaps.init())
-      // .pipe(sass().on("error", sass.logError))
-      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-      .pipe(
-        autoprefixer(
-          "last 2 version",
-          "safari 5",
-          "ie 8",
-          "ie 9",
-          "opera 12.1",
-          "ios 6",
-          "android 4"
-        )
+function cssBuild() {
+  let options;
+
+  if (argv.development) {
+    options = {
+      outputStyle: "expanded",
+    };
+  } else {
+    options = {
+      outputStyle: "compressed",
+    };
+  }
+
+  return gulp
+    .src(SRC_PATH + "/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass(options).on("error", sass.logError))
+    .pipe(
+      autoprefixer(
+        "last 2 version",
+        "safari 5",
+        "ie 8",
+        "ie 9",
+        "opera 12.1",
+        "ios 6",
+        "android 4"
       )
-      .pipe(sourcemaps.write("."))
-      .pipe(rename({ suffix: ".min" }))
-      .pipe(gulp.dest(BUILD_PATH))
-  );
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest(BUILD_PATH));
 }
 
 // function jsBuild() {
@@ -88,6 +98,12 @@ function jsBuild(cb) {
     .pipe(gulp.dest(BUILD_PATH));
 }
 
+function watch() {
+  gulp.watch(SRC_PATH + "/**/*.scss", cssBuild);
+  gulp.watch(SRC_PATH + "/**/*.js", jsBuild);
+}
+
 exports.clean = clean;
+exports.watch = watch;
 
 exports.build = gulp.series(clean, gulp.parallel(cssBuild, jsBuild));
